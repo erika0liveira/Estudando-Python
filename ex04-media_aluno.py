65d88eb973eff1307e0be913a721027ff92f37ba
erika <erikaalves018@gmail.com>
2021-02-07 04:36:54 -0300

    feito com array

--- ex04-media_aluno.py
+++ ex04-media_aluno.py
@@ -1,12 +1,7 @@
 #Faça um programa que leia a média de duas notas
-#nome=str(input('Digite o nome do aluno: '))
 
-#n1=float(input('Primeira nota: '))
-#n2=float(input('Segunda nota: '))
+nome = str(input('Digite o nome do aluno: '))
 
-#print('A média do(a) aluno(a) {} é: {}'.format(nome, ((n1+n2)/2)))
-
-#Tentativa com Array, média do aluno contando 5 notas
 notas = list(map(float, input('Digite as 5 notas do aluno separadas por espaço: ').split()))
 
 for i in range (len(notas)):
