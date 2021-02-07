ff8b2c36cbb5b866cfef5aa30159f228ef1fdc8f
erika <erikaalves018@gmail.com>
2021-02-07 05:01:27 -0300

    exercicio área total

--- ex08-tinta.py
+++ ex08-tinta.py
@@ -3,7 +3,7 @@
 larg = float(input('Digite a largura da parede em metros: '))
 altura = float(input('Digite a altura da parede em metros: '))
 
-area = (larg*altura)
+area = larg * altura
 tinta = area/2
 
 print('\nA parede tem a seguinte dimensão: {}X{}, dessa forma a área total é: {:.2f} mt².\nSera necessário, aproximadamente, {:.2f} litro(s) de tinta'.format(larg, altura, area, tinta))
