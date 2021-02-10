380728c67136cfae5d5dbdac8a23b84f5b970cad
erika <erikaalves018@gmail.com>
2021-02-10 00:52:27 -0300

    alinhado variáveis de input

--- ex06-tabuada.py
+++ ex06-tabuada.py
@@ -4,5 +4,5 @@ max=int(input('Quer multiplica-lo por quantas vezes? '))
 print('\n')
 
 for i in range (max+1):
-    print('{} X {} = {}'.format(num, i, (num*i)))
+    print('{} X {:2} = {:2}'.format(num, i, (num*i)))
     i+=1
