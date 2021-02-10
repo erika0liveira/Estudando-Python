8822450e5d037f5cb3d2d49a26bcd0fdffeed507
erika <erikaalves018@gmail.com>
2021-02-10 01:03:22 -0300

    otimizado

--- ex08-tinta.py
+++ ex08-tinta.py
@@ -3,8 +3,5 @@
 larg = float(input('Digite a largura da parede em metros: '))
 altura = float(input('Digite a altura da parede em metros: '))
 
-area = larg * altura
-tinta = area/2
-
-print('\nA parede tem a seguinte dimensão: {}X{}, dessa forma a área total é: {:.2f} mt².\nSera necessário, aproximadamente, {:.2f} litro(s) de tinta'.format(larg, altura, area, tinta))
+print('\nA parede tem a seguinte dimensão: {} X {}, dessa forma a área total é: {:.2f} mt².\nSera necessário, aproximadamente, {:.1f} litro(s) de tinta'.format(larg, altura, (larg*altura), ((larg*altura)/2)))
 
