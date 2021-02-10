c8ef34ddcb83e537755fd0cc4aa4d7d631e6847d
erika <erikaalves018@gmail.com>
2021-02-10 00:46:58 -0300

    conversor

--- ex05-conversor_medidas.py
+++ ex05-conversor_medidas.py
@@ -1,5 +1,5 @@
 # Converter metro para centémetros e milímetros
 metro=int(input('Digite a metragem a ser convertida: '))
 
-print('{} metro(s) em centímetros: {}\n{} metro(s) em milímetros: {}'.format(metro, (metro*100), metro, (metro*1000)))
+print('{} metro(s) em centímetros: {:.2f}\n{} metro(s) em milímetros: {:.2f}'.format(metro, (metro*100), metro, (metro*1000)))
 
