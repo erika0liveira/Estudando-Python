38404d0b4b11a53fcca0a6ca70ecfd203853b088
erika <erikaalves018@gmail.com>
2021-02-11 22:33:18 -0300

    codigo otimizado

--- Mundo 01/02 - Modulo/ex14-sorteio_aluno.py
+++ Mundo 01/02 - Modulo/ex14-sorteio_aluno.py
@@ -1,13 +1,13 @@
-import random
+from random import choice, shuffle
 
 #Sortear aluno para limpar louse
-nome = list(input('Digite os 4 nomes para serem sorteados: ').split(', '))
-print('\nO aluno escolhido para limpar a lousa foi: {}!\n'.format(random.choice(nome)))
+nome = list(input('Digite os nomes para serem sorteados: ').split(', '))
+print('\nO aluno escolhido para limpar a lousa foi: {}!\n'.format(choice(nome)))
 
 #Sortear ordem de apresentação
 print('A ordem para apresentação dos trabalhos será:')
 
-random.shuffle(nome)
+shuffle(nome)
 
 for i in range (len(nome)):
     print('{}° - {}'.format(i+1,nome[i]))
