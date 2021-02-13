98fe53ea6ec439850ea4de627a921754047695dc
erika <erikaalves018@gmail.com>
2021-02-13 18:56:42 -0300

    adicionado sleep e otimizado imports

--- Mundo 01/03 - Condicoes_PT01/ex19-numero_aleatorio.py
+++ Mundo 01/03 - Condicoes_PT01/ex19-numero_aleatorio.py
@@ -1,13 +1,17 @@
-import random
+from random import randint
+from time import sleep
 
 print('\n********* ADVINHE O NÚMERO **********')
 
 num = int(input('\nEscolha um número entre 0 e 10: '))
-numPC = random.randint(0, 10)
+numPC = randint(0, 10)
 
-print('\nO número escolhido pelo computador foi: {}.'.format(numPC))
+print('Processando...')
+sleep(1)
 
 if num == numPC:
     print('\nVocê acertou, parabéns!')
 else:
     print('\nQue pena, não foi dessa vez!')
+
+print('O número escolhido pelo computador foi: {}.'.format(numPC))
