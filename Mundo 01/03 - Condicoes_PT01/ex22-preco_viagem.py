3c6aa2700278efdc5923e13b6be413bb0f677d33
erika <erikaalves018@gmail.com>
2021-02-13 19:15:09 -0300

    adicionado metodo simplificado para calcular o valor

--- Mundo 01/03 - Condicoes_PT01/ex22-preco_viagem.py
+++ Mundo 01/03 - Condicoes_PT01/ex22-preco_viagem.py
@@ -1,6 +1,13 @@
 distancia = float(input('Informe a distância em KM da viagem: '))
 
+#Maneira "convencional"
 if distancia <= 200:
-    print('\nO preço desta viagem de {} KM será de R$ {:.2f}'.format(distancia, distancia*0.50))
+    print('\nO preço desta viagem de {} KM será de R$ {:.2f}'.format(distancia, distancia * 0.50))
 else:
-    print('\nO preço desta viagem de {} KM  será de R$ {:.2f}'.format(distancia, distancia * 0.45))
+    print('\nO preço desta viagem de {} KM será de R$ {:.2f}'.format(distancia, distancia * 0.45))
+
+'''#Maneira simplificada
+preco = distancia * 0.50 if distancia <= 200 else distancia * 0.45
+print('\nO preço desta viagem de {} KM será de R$ {:.2f}'.format(distancia, preco))'''
+
+
