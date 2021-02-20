7bf55a32ca5042177c6ae2a08cf9cfd72747fd46
erika <erikaalves018@gmail.com>
2021-02-20 00:41:48 -0300

    soma de numeros e 999 para encerrar

---
+++ Mundo-02/ex64-999.py
@@ -0,0 +1,10 @@
+n = 0
+c = []
+
+print('\n{:-^50}'.format(' SOMA DE VÁRIOS NÚMEROS '))
+
+while n != 999:
+    n = int(input('\nDigite números para somar ou 999 para parar o programa: '))
+    if n != 999:
+        c.append(n)
+print('\nVocê digitou {} números {} que somados resultam em {}'.format(len(c), c, sum(c)))
