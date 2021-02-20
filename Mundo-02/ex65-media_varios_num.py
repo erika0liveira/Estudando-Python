2ca06ab9793d7152bd25d09f9fa4b840179e6166
erika <erikaalves018@gmail.com>
2021-02-20 00:49:24 -0300

    calculos com while

--- Mundo-02/ex65-media_varios_num.py
+++ Mundo-02/ex65-media_varios_num.py
@@ -7,7 +7,8 @@ while continua == 'S':
     continua = str(input('Quer Ler mais um número? [S/N]: ')).upper()
     if continua != 'S':
         print('\n{:-^100}'.format(' RESULTADOS '))
-        print('\nVocê digitou {} números {} que somados resulta em {}'.format(len(c), c, sum(c)))
+        print('\nVocê digitou {} números {}'.format(len(c), c))
+        print('Soma: {}'.format(sum(c)))
         print('Média {:.1f}'.format((sum(c)) / len(c)))
         print('Maior número: {}\nMenor número: {}'.format(max(c), min(c)))
 
