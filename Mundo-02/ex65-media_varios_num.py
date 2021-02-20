46a17d0b5b2e1642cb1ac3d3d6732a3079622b43
erika <erikaalves018@gmail.com>
2021-02-20 00:43:09 -0300

    soma de números com while

---
+++ Mundo-02/ex65-media_varios_num.py
@@ -0,0 +1,13 @@
+continua = 'S'
+c = []
+
+while continua == 'S':
+    n = int(input('\nDigite um número: '))
+    c.append(n)
+    continua = str(input('Quer Ler mais um número? [S/N]: ')).upper()
+    if continua != 'S':
+        print('\n{:-^100}'.format(' RESULTADOS '))
+        print('\nVocê digitou {} números {} que somados resulta em {}'.format(len(c), c, sum(c)))
+        print('Média {:.1f}'.format((sum(c)) / len(c)))
+        print('Maior número: {}\nMenor número: {}'.format(max(c), min(c)))
+
