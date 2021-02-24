53bd8464fc5e5ae6391dc9b74c4d3b35c29fe26c
erika <erikaalves018@gmail.com>
2021-02-24 02:50:23 -0300

    varios numeros com flag

--- Mundo-02/ex64-999.py
+++ Mundo-02/ex64-999.py
@@ -5,6 +5,8 @@ print('\n{:-^50}'.format(' SOMA DE VÁRIOS NÚMEROS '))
 
 while n != 999:
     n = int(input('\nDigite números para somar ou 999 para encerrar o programa: '))
-    if n != 999:
+    if n == 999:
+        break
+    else:
         c.append(n)
 print('\nVocê digitou {} números {} que somados resultam em {}'.format(len(c), c, sum(c)))
