c656aa2b8f097307fe0f86448306c4c338453a29
erika <erikaalves018@gmail.com>
2021-03-20 19:41:02 -0300

    ex 112

---
+++ Tratamento_Erros/design/__init__.py
@@ -0,0 +1,9 @@
+def seplin():
+    print('-' * 100)
+
+
+def titulo(msg):
+    tam = len(msg)
+    print('-' * (tam + 4))
+    print(f'  {msg:^{tam}}  ')
+    print('-' * (tam + 4))
---
+++ Tratamento_Erros/ex112-leia_int_float.py
@@ -0,0 +1,4 @@
+from Tratamento_Erros import dado
+
+dado.leint(input('Digite um número INTEIRO: '))
+dado.lefloat(input('Digite um número FLOAT: '))
